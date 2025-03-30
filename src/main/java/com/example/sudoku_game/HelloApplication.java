f439cd990f67002f4cb66991ec803dbfdf6b7431
Nicolle Paz <nicolleePaz16>
2025-03-30 16:52:24 -0500

    Declaration of label and buttons

--- src/main/java/com/example/sudoku_game/HelloApplication.java
+++ src/main/java/com/example/sudoku_game/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/sudoku_game/menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 443, 328);
-        stage.setTitle("Hello!");
+        stage.setTitle("Menú");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/sudoku_game/controllers/MenuController.java
+++ src/main/java/com/example/sudoku_game/controllers/MenuController.java
@@ -2,8 +2,22 @@ package com.example.sudoku_game.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class MenuController {
+
+    @FXML
+    private Label SudokuGameLabe;
+
+    @FXML
+    private Button Iniciar;
+
+    @FXML
+    private Button Reiniciar;
+
+    @FXML
+    private Button Salir;
 
     @FXML
     void onActionExitButtom(ActionEvent event) {
@@ -21,3 +35,4 @@ public class MenuController {
     }
 
 }
+
