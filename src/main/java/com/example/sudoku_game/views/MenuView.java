ab4a5787c3102d77714d8ff68a03eb606464b401
Nicolle <nicolle.paz@correounivalle.edu.co>
2025-04-06 19:09:54 -0500

    MenuView and Main implementation

--- src/main/java/com/example/sudoku_game/views/MenuView.java
+++ src/main/java/com/example/sudoku_game/views/MenuView.java
@@ -1,4 +1,36 @@
 package com.example.sudoku_game.views;
 
-public class MenuView {
+import com.example.sudoku_game.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuView extends Stage {
+
+    public MenuView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Main.class.getResource("menu-view.fxml")
+        );
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 443, 328);
+        this.setTitle("Menú");
+        this.setScene(scene);
+    }
+
+    public static MenuView getInstance() throws IOException {
+        if (MenuViewHolder.INSTANCE == null) {
+            MenuViewHolder.INSTANCE = new MenuView();
+            return MenuViewHolder.INSTANCE;
+        } else {
+            return MenuViewHolder.INSTANCE;
+        }
+    }
+
+    private static class MenuViewHolder {
+        private static MenuView INSTANCE;
+    }
+
 }
